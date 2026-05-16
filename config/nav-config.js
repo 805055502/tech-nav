0083577d4da2a11881020a15f2cb4825e3d6ee11
11 <qq805055502@gmail.com>
2026-05-16 18:16:23 +0800

    整理

--- config/nav-config.js
+++ config/nav-config.js
@@ -58,7 +58,7 @@ let navData = {
                 title: '币安',
                 desc: '全球加密货币交易量最大的平台',
                 url: 'https://accounts.binance.com/register?ref=YPIOJFIL',
-                logoUrl: '../assert/ico/binance.png',
+                logoUrl: './assert/ico/binance.png',
                 keywords: '币安 binance 交易所 加密货币',
                 highlight: false,
                 recommended: false
@@ -67,7 +67,7 @@ let navData = {
                 title: 'OKX',
                 desc: '欧易OKX是全球领先的加密货币交易平台和加密生态建设者',
                 url: 'https://www.okx.com/join/2638599',
-                logoUrl: '../assert/ico/okex.png',
+                logoUrl: './assert/ico/okex.png',
                 keywords: 'okx 欧意 交易所 加密货币',
                 highlight: false,
                 recommended: false
@@ -76,7 +76,7 @@ let navData = {
                 title: 'Bitget',
                 desc: '成立于2018年，是世界领先的加密货币交易所和 Web 3 公司',
                 url: 'https://www.bitget.com/zh-CN/referral/register?clacCode=0WN4DU1L',
-                logoUrl: '../assert/ico/bitget.png',
+                logoUrl: './assert/ico/bitget.png',
                 keywords: 'Bitget 交易所 加密货币',
                 highlight: false,
                 recommended: false
@@ -87,7 +87,7 @@ let navData = {
                 title: 'polymarket',
                 desc: '全球最大的预测市场平台',
                 url: 'https://polymarket.com/zh',
-                logoUrl: '../assert/ico/ploymarket.png',
+                logoUrl: './assert/ico/ploymarket.png',
                 keywords: 'polymarket 预测',
                 highlight: false,
                 recommended: false
@@ -98,7 +98,7 @@ let navData = {
                 title: 'GMGN',
                 desc: '链上打狗必备，更快发现meme币',
                 url: 'https://gmgn.ai/r/Mv6xCqTr',
-                logoUrl: '../assert/ico/gmgn.svg',
+                logoUrl: './assert/ico/gmgn.svg',
                 keywords: 'gmgn dex',
                 highlight: false,
                 recommended: false
@@ -109,7 +109,7 @@ let navData = {
                 title: 'defillama',
                 desc: 'DeFi 领域最权威、最全面的 TVL（Total Value Locked，总锁仓价值）聚合与数据分析平台',
                 url: 'https://defillama.com/',
-                logoUrl: '../assert/ico/defillama.svg',
+                logoUrl: './assert/ico/defillama.svg',
                 keywords: '数据 分析 defillama',
                 highlight: false,
                 recommended: false
@@ -118,7 +118,7 @@ let navData = {
                 title: 'rootdata',
                 desc: '领先的 Web3 / Crypto 项目资产数据平台（类似 Crypto 项目“百科全书 + 投融资数据库”）',
                 url: 'https://cn.rootdata.com/',
-                logoUrl: '../assert/ico/rootdata.png',
+                logoUrl: './assert/ico/rootdata.png',
                 keywords: 'rootdata 百科全书 数据平台',
                 highlight: false,
                 recommended: false
@@ -127,7 +127,7 @@ let navData = {
                 title: 'Arkham',
                 desc: '区块链链上情报与去匿名化分析平台（常被称为“加密货币领域的彭博终端”）',
                 url: 'https://zh.arkhamintelligence.com/',
-                logoUrl: '../assert/ico/aka.png',
+                logoUrl: './assert/ico/aka.png',
                 keywords: 'Arkham 分析平台',
                 highlight: false,
                 recommended: false
@@ -145,7 +145,7 @@ let navData = {
                 title: 'DeBank',
                 desc: '领先的多链 DeFi 投资组合跟踪器',
                 url: 'https://debank.com/',
-                logoUrl: '../assert/ico/debank.svg',
+                logoUrl: './assert/ico/debank.svg',
                 keywords: '追踪 debank',
                 highlight: false,
                 recommended: false
@@ -154,7 +154,7 @@ let navData = {
                 title: 'Dune',
                 desc: '区块链/加密货币领域最强大、最灵活的链上数据分析平台，被誉为“Web3 的数据 GitHub”或“链上数据的 Tableau”',
                 url: 'https://dune.com/',
-                logoUrl: '../assert/ico/dune.png',
+                logoUrl: './assert/ico/dune.png',
                 keywords: 'Dune 链上数据',
                 highlight: false,
                 recommended: false
@@ -163,7 +163,7 @@ let navData = {
                 title: 'discover',
                 desc: '一个 Web3 / Crypto 项目社交分析平台，专注于早期发现新项目',
                 url: 'https://discover.getmoni.io/',
-                logoUrl: '../assert/ico/discover.png',
+                logoUrl: './assert/ico/discover.png',
                 keywords: 'discover 监控 追踪 KOL 新项目',
                 highlight: false,
                 recommended: false
@@ -186,7 +186,7 @@ let navData = {
             title: 'tokennav',
             desc: '收录了一百多个中转站，可以按模型、计费、支付方式快速筛选',
             url: 'https://tokennav.cc/',
-            logoUrl: '../assert/ico/website.png',
+            logoUrl: './assert/ico/website.png',
             keywords: 'tokennav',
             highlight: false,
             recommended: false
@@ -198,56 +198,56 @@ let navData = {
             title: 'openclaw',
             desc: ' 2026 年最火的开源个人 AI Agent 框架（也被称为“本地运行的私人 AI 助手”），常被戏称为“小龙虾”',
             url: 'https://github.com/openclaw/openclaw',
-            logoUrl: '../assert/ico/openclaw.jpg',
+            logoUrl: './assert/ico/openclaw.jpg',
             keywords: 'openclaw agent'
         },
         {
             title: 'hermes',
             desc: ' Nous Research 推出的开源自改进（Self-Improving）AI Agent 框架，被定位为“和你一起成长的 Agent”',
             url: 'https://github.com/NousResearch/hermes-agent',
-            logoUrl: '../assert/ico/hermes.png',
+            logoUrl: './assert/ico/hermes.png',
             keywords: 'hermes agent'
         },
         {
             title: 'BettaFish',
             desc: ' 人人可用的多Agent舆情分析助手，打破信息茧房，还原舆情原貌，预测未来走向，辅助决策',
             url: 'https://github.com/666ghj/BettaFish',
-            logoUrl: '../assert/ico/BettaFish.png',
+            logoUrl: './assert/ico/BettaFish.png',
             keywords: 'BettaFish 舆情 决策'
         },
         {
             title: 'OpenCLI',
             desc: '一个将任意网站、Electron 桌面应用或本地工具转化为标准化 CLI 的通用 CLI Hub 和 AI 原生运行时',
             url: 'https://github.com/jackwener/OpenCLI',
-            logoUrl: '../assert/ico/opencli.jpg',
+            logoUrl: './assert/ico/opencli.jpg',
             keywords: 'OpenCLI CLI'
         },
         {
             title: 'sub2api',
             desc: '一站式开源中转服务，让 Claude、Openai 、Gemini、Antigravity订阅统一接入',
             url: 'https://github.com/Wei-Shaw/sub2api',
-            logoUrl: '../assert/ico/website.png',
+            logoUrl: './assert/ico/website.png',
             keywords: 'sub2api'
         },
         {
             title: 'new-api',
             desc: '基于sub2api二次开发的自托管 AI 模型聚合、分发和管理系统',
             url: 'https://github.com/QuantumNous/new-api',
-            logoUrl: '../assert/ico/website.png',
+            logoUrl: './assert/ico/website.png',
             keywords: 'new-api'
         },
         {
             title: 'RuView',
             desc: '能把普通 WiFi 信号转化为实时空间智能、无摄像头人体检测与姿态估计系统',
             url: 'https://github.com/ruvnet/RuView',
-            logoUrl: '../assert/ico/website.png',
+            logoUrl: './assert/ico/website.png',
             keywords: 'RuView wifi'
         },
         {
             title: 'AiToEarn',
             desc: '帮助 OPC（一人公司）、创作者、品牌与企业在全球主流平台上构建、分发并变现内容。',
             url: 'https://github.com/yikart/AiToEarn',
-            logoUrl: '../assert/ico/aitoearn.png',
+            logoUrl: './assert/ico/aitoearn.png',
             keywords: 'AiToEarn'
         }
     ],
@@ -267,7 +267,7 @@ let navData = {
                 title: 'FLAC Downloader',
                 desc: '免费下载FLAC无损音乐，音质直接拉满',
                 url: 'https://flacdownloader.com/',
-                logoUrl: '../assert/ico/website.png',
+                logoUrl: './assert/ico/website.png',
                 keywords: '音乐 无损 flac downloader'
             }
         ]
@@ -279,7 +279,7 @@ let navData = {
                 title: 'Wormhole',
                 desc: 'Wormhole 让你能以端到端加密和自动过期链接的方式分享文件。 所以你可以确保你分享的文件私密且不会一直留在互联网上。</br></br>对于5GB以内的文件，Wormhole将在服务器上存储24小时。</br><br>对于大于5GB的文件，Wormhole使用点对点传输，直接从你的浏览器向接收者发送文件。所以你需要保持页面打开直到接收者下载完毕。',
                 url: 'https://wormhole.app',
-                logoUrl: '../assert/ico/website.png',
+                logoUrl: './assert/ico/website.png',
                 keywords: 'Wormhole 文件传输 文件上传 不限速 快传 端到端 加密传输'
             }
         ],
@@ -311,14 +311,14 @@ let navData = {
                 title: "88查",
                 desc: "免费查企业",
                 url: "https://88cha.com/",
-                logoUrl: '../assert/ico/88cha.png',
+                logoUrl: './assert/ico/88cha.png',
                 keywords: "88查 企业背调 商标"
             },
             {
                 title: 'appstoreprice',
                 desc: '实时抓取并对比全球各大区 App Store 的应用价格',
                 url: 'https://appstoreprice.org/zh',
-                logoUrl: '../assert/ico/website.png',
+                logoUrl: './assert/ico/website.png',
                 keywords: 'appstoreprice',
                 highlight: false,
                 recommended: false
@@ -327,7 +327,7 @@ let navData = {
                 title: 'publicapis',
                 desc: '收集了1500多个免费api',
                 url: 'https://publicapis.io/',
-                logoUrl: '../assert/ico/website.png',
+                logoUrl: './assert/ico/website.png',
                 keywords: 'publicapi 免费api'
             },
         ],
